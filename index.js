9b73a2764609fc2435651027dd6708e422dbdc1a
jordles113 <jordan.t.romero@gmail.com>
2021-03-19 13:09:50 -0500

    working on mapping and dynamically rendering data

--- index.js
+++ index.js
@@ -28,32 +28,51 @@ const url = require('url');
 // });
 
 ///SERVER///
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+};
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-
   const pathname = req.url;
-
-  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-
-  const tempOverview = fs.readFileSync(
-    `${__dirname}/templates/template-overview.html`,
-    'utf-8'
-  );
-
-  const tempCard = fs.readFileSync(
-    `${__dirname}/templates/template-card.html`,
-    'utf-8'
-  );
-
-  const tempProduct = fs.readFileSync(
-    `${__dirname}/templates/template-product.html`,
-    'utf-8'
-  );
 
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
     res.end(tempOverview);
   } else if (pathname === '/product') {
     res.end('Hello from the product!');
